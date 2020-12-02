7d39b0d537bc74bcb0064eff719379e143cc8bd5
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-12-02 14:25:41 -0800

    null check

--- pctsea-cl/src/main/java/edu/scripps/yates/pctsea/PCTSEACommandLine.java
+++ pctsea-cl/src/main/java/edu/scripps/yates/pctsea/PCTSEACommandLine.java
@@ -287,12 +287,14 @@ public class PCTSEACommandLine extends CommandLineProgramGuiEnclosable {
 
 	private String getAvailableDatasets() {
 		final StringBuilder sb = new StringBuilder();
-		final List<Dataset> findAll = this.dmr.findAll();
-		for (final Dataset dataset : findAll) {
-			if (!"".equals(sb.toString())) {
-				sb.append(",");
+		if (this.dmr != null) {
+			final List<Dataset> findAll = this.dmr.findAll();
+			for (final Dataset dataset : findAll) {
+				if (!"".equals(sb.toString())) {
+					sb.append(",");
+				}
+				sb.append(dataset.getTag());
 			}
-			sb.append(dataset.getTag());
 		}
 		return sb.toString();
 	}
