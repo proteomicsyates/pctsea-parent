adda23bb4343ba4003a1a1543cb0181ff3cb9ce4
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2021-02-25 16:05:52 -0800

    show error if table format is not as expected

--- pctseaweb/shinyR/server/Table.R
+++ pctseaweb/shinyR/server/Table.R
@@ -19,6 +19,10 @@ enrichment_table <- eventReactive(enrichment_file(),{
     setProgress(value = 0, message = "Reading enrichment table...")
     t <- fread(file = enrichment_file(), header = TRUE, skip = 33,  sep = "\t", fill = TRUE) # IMPORTANT: 33 is the number of rows to skip until finding the actual table on the file
     setProgress(value = 0.5)
+    if (!("ews" %in% colnames(t) )){
+      showNotification(paste("Error reading file '",enrichment_file(),","), duration = NULL, type = "error")
+      return(NULL)
+    }
     # take only with positives ews:
     t = t[ews>0,]
     # replace all '_' by spaces on names
@@ -32,18 +36,21 @@ enrichment_table <- eventReactive(enrichment_file(),{
 
 # plot the table as soon as is loaded
 output$enrichmentDataTable <- DT::renderDT(
-  datatable(
-    enrichment_table(),
-    filter = 'top',
-    selection = 'single',
-    options = list(
-      pageLength = 10,
-      columnDefs = list(list(className = 'dt-center', targets = 5)),
-      order = list(list(13, 'asc'), list(12, 'asc'), list(20, 'asc'))
-    )
-  ) %>%
-    formatRound(columns=c("hyperG p-value", "FDR", "empirical p-value", "KS p-value", "KS p-value BH corrected"), digits=4) %>%
-    formatRound(columns=c("log2 ratio", "ews", "2nd ews", "norm-ews", "norm-supX", "2nd supX", "Dab", "Umap x", "Umap y"), digits=2)
+  {
+    req(enrichment_table())
+    datatable(
+      enrichment_table(),
+      filter = 'top',
+      selection = 'single',
+      options = list(
+        pageLength = 10,
+        columnDefs = list(list(className = 'dt-center', targets = 5)),
+        order = list(list(13, 'asc'), list(12, 'asc'), list(20, 'asc'))
+      )
+    ) %>%
+      formatRound(columns=c("hyperG p-value", "FDR", "empirical p-value", "KS p-value", "KS p-value BH corrected"), digits=4) %>%
+      formatRound(columns=c("log2 ratio", "ews", "2nd ews", "norm-ews", "norm-supX", "2nd supX", "Dab", "Umap x", "Umap y"), digits=2)
+  }
 )
 
 # having the table, enable us to get the unique cell types
@@ -110,7 +117,7 @@ output$enrichmentDataTableForCluster <- DT::renderDT(
 #   table <- enrichment_table()
 #   req(table)
 #   selected_choice <- table[[selected_cell_type,"cell type"]]
-#   
+#
 # }
 # )
 
